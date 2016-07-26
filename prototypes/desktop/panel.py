f4cdc6f0235067297473e3fa685b454358be8500
Tiffany J <tiffanyj@iqt.org>
2016-07-25 17:58:32 -0700

    refactor panel.py

--- prototypes/desktop/panel.py
+++ prototypes/desktop/panel.py
@@ -4,23 +4,31 @@ import psycopg2.extras
 import web
 import os
 
-# TODO: update url
 urls = (
     
-    # rest API backend endpoints
-    "panel/", "all_panels",
-    "panel/(.*)/(.*)/", "single_panel",
+    # 127.0.0.1:8000/api/panel/
+    "", "all_panels",
+    # 127.0.0.1:8000/api/panel/#/, where # == panel.id
+    "(\d+)/", "single_panel",
+    # 127.0.0.1:8000/api/panel/persona/#/, where # == persona.id
+    "persona/(\d+)/", "persona_panels",
     
 )
     
 class all_panels:
+    """ Extract all the panels.
+    output:
+        * panel.id
+        * panel.name
+        * panel.url_name
+    """
     def GET(self, connection_string=os.environ['DATABASE_URL']):
         # connect to postgresql based on configuration in connection_string
         connection = psycopg2.connect(connection_string)
         # get a cursor to perform queries
         self.cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
         # execute query
-        cursor.execute("""
+        self.cursor.execute("""
             SELECT * FROM panel;
         """)
         # obtain the data
@@ -28,19 +36,59 @@ class all_panels:
         # convert data to a string
         return json.dumps(data)
     
-# TODO: what is this? get this working!
 class single_panel:
-    def GET(self, panel_type, panel_id):
+    """ Extract a panel with a specific id.
+    input:
+        * panel.id
+    output:
+        * panel.id
+        * panel.name
+        * panel.url_name
+    """
+    def GET(self, panel_id, connection_string=os.environ['DATABASE_URL']):
+        # connect to postgresql based on configuration in connection_string
+        connection = psycopg2.connect(connection_string)
+        # get a cursor to perform queries
+        self.cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)    
+        # execute query
+        self.cursor.execute("""
+            SELECT * 
+            FROM panel
+            WHERE panel.id = """ + panel_id + """;
+        """)
+        # obtain the data
+        data = self.cursor.fetchall()
+        # convert data to a string
+        return json.dumps(data)
+
+class persona_panels:
+    """ Extract all the panels for a particular persona.
+    input:
+        * persona.id
+    output:
+        * panel.id
+        * panel.name
+        * panel.url_name
+    """
+    def GET(self, persona_id, connection_string=os.environ['DATABASE_URL']):
         # connect to postgresql based on configuration in connection_string
         connection = psycopg2.connect(connection_string)
         # get a cursor to perform queries
         self.cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
         # execute query
-        cursor.execute("""select t.* from gestalt_""" + panel_type + """ t where t.param = """ + panel_id + """;""")
-        
-        # get rows
-        data = cursor.fetchall()
-        
+        self.cursor.execute("""
+            SELECT DISTINCT ON (pa.id) pa.id, pa.name, pa.url_name 
+            FROM panel pa
+            RIGHT JOIN persona_panel_story pcs
+            ON pa.id = pcs.panel_id 
+            AND pcs.persona_id = """ + persona_id + """
+            WHERE pa.id IS NOT NULL
+            ORDER BY pa.id;
+        """)        
+        # obtain the data
+        data = self.cursor.fetchall()
+        # convert data to a string
         return json.dumps(data)
-    
+
+# instantiate the application
 app = web.application(urls, locals())
