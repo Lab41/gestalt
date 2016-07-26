130067b864e8423428085001b9920978d752caa0
Tiffany J <tiffanyj@iqt.org>
2016-07-26 13:51:55 -0700

    add query for workspace's tags

--- prototypes/desktop/tag.py
+++ prototypes/desktop/tag.py
@@ -13,11 +13,15 @@ urls = (
     # 127.0.0.1:8000/api/tag/story/#/, where # == story.id
     "story/(\d+)/", "single_story_tags",
     # 127.0.0.1:8000/api/tag/#/story/, where # == tag.id
-    "(\d+)/story/", "all_stories_with_tag",
+    "(\d+)/stories/", "all_stories_with_tag",
     # 127.0.0.1:8000/api/tag/panel/#/, where # == panel.id
     "panel/(\d+)/", "single_panel_tags",
     # 127.0.0.1:8000/api/tag/#/panel/, where # == tag.id
-    "(\d+)/panel/", "all_panels_with_tag"
+    "(\d+)/panels/", "all_panels_with_tag",
+    # 127.0.0.1:8000/api/tag/workspace/#/, where # == workspace.id
+    "workspace/(\d+)/", "single_workspace_tags",
+    # 127.0.0.1:8000/api/tag/#/workspace/, where # == tag.id
+    "(\d+)/workspaces/", "all_workspaces_with_tag"
 
 )
         
@@ -123,7 +127,7 @@ class all_stories_with_tag:
         return json.dumps(data)
 
 class single_panel_tags:
-    """ Extract all the tags from a single story.
+    """ Extract all the tags from a single panel.
     input:
         * panel.id
     output:
@@ -179,6 +183,66 @@ class all_panels_with_tag:
         # convert data to a string
         return json.dumps(data)
 
+class single_workspace_tags:
+    """ Extract all the tags from a single workspace.
+    input:
+        * workspace.id
+    output:
+        * tag.id
+        * tag.name
+    """
+    def GET(self, workspace_id, connection_string=os.environ['DATABASE_URL']):
+        # connect to postgresql based on configuration in connection_string
+        connection = psycopg2.connect(connection_string)
+        # get a cursor to perform queries
+        self.cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)    
+        # execute query
+        self.cursor.execute("""
+            SELECT DISTINCT ON (tag.id) tag.id, tag.name
+            FROM tag
+            RIGHT JOIN workspace_tag
+            ON tag.id = workspace_tag.tag_id 
+            AND workspace_tag.workspace_id = """ + workspace_id + """
+            WHERE tag.id IS NOT NULL
+            ORDER BY tag.id;
+        """)
+        # obtain the data
+        data = self.cursor.fetchall()
+        # convert data to a string
+        return json.dumps(data)
 
+class all_workspaces_with_tag:
+    """ Extract all the workspaces with a particular tag.
+    input:
+        * tag.id
+    output:
+        * workspace.id
+        * workspace.name
+        * persona.name        
+        * workspace.url_name
+    """
+    def GET(self, tag_id, connection_string=os.environ['DATABASE_URL']):
+        # connect to postgresql based on configuration in connection_string
+        connection = psycopg2.connect(connection_string)
+        # get a cursor to perform queries
+        self.cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)    
+        # execute query
+        self.cursor.execute("""
+            SELECT DISTINCT ON (w.id) w.id, wn.name, p.name AS persona_name, w.url_name
+            FROM workspace w
+            LEFT JOIN workspace_name wn
+            ON w.workspace_name_id = wn.id
+            LEFT JOIN persona p
+            ON w.persona_id = p.id
+            RIGHT JOIN workspace_tag wt
+            ON w.id = wt.workspace_id 
+            AND wt.tag_id = """ + tag_id + """
+            WHERE w.id IS NOT NULL
+            ORDER BY w.id;
+        """)
+        # obtain the data
+        data = self.cursor.fetchall()
+        # convert data to a string
+        return json.dumps(data)
 # instantiate the application
 app = web.application(urls, locals())
