91dfb38f8276e2d6b53226e689cb2a4965693ed9
Tiffany J <tiffapedia@gmail.com>
2016-08-26 16:32:40 -0700

    create individual table for each visualization's attributes

--- database/create_vis.sql
+++ database/create_vis.sql
@@ -5,86 +5,114 @@
  * which visualization works best for which dataset.
  *
  * To do this, we will use the following format: 
- * panel == refers to the dataset type (i.e. time series)
- * story == name of visualization
+ * panel == visualization's type (i.e. time series) from vis_type
+ * story == visualizatoin's name from vis
  *
- * When you go through each panel for the different dataset type,
- * you can see a list of visualizations that are suited for that
- * particular dataset type.
+ * When you go through each panel for the different visualization 
+ * type, you can see a list of visualizations that falls under the
+ * visualization type.
  * 
  */
 
-/* dataset_type ------------------------------------------------------------ */
+/* vis_type ------------------------------------------------------------ */
 
-CREATE TABLE dataset_type (
+CREATE TABLE vis_type (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL CHECK (name <> ''),
     UNIQUE (name)
 );
 
--- Insert new dataset type into panel as well.
--- TODO: Is there a better way to do this?
+INSERT INTO vis_type (name) VALUES ('comparison');
+INSERT INTO vis_type (name) VALUES ('time series');
+INSERT INTO vis_type (name) VALUES ('hierarchy');
+INSERT INTO vis_type (name) VALUES ('parts of a whole');
+INSERT INTO vis_type (name) VALUES ('relatedness');
 
-WITH inserted_dataset_type AS (
-    INSERT INTO dataset_type (name) VALUES 
-    ('Comparison') 
-    RETURNING name AS dataset_type_name
+/*
+
+-- TODO: Is there a better way to do this than reinserting the same values in the panel table? 
+
+WITH inserted_vis_type AS (
+    INSERT INTO vis_type (name) VALUES 
+    ('comparison') 
+    RETURNING name AS vis_type_name
 )
 INSERT INTO panel (name, url_name) 
-SELECT dataset_type_name, md5(random()::text)
-FROM inserted_dataset_type;
+SELECT vis_type_name, md5(random()::text)
+FROM inserted_vis_type;
 
-WITH inserted_dataset_type AS (
-    INSERT INTO dataset_type (name) VALUES
-    ('Time series')
-    RETURNING name AS dataset_type_name
+WITH inserted_vis_type AS (
+    INSERT INTO vis_type (name) VALUES
+    ('time series')
+    RETURNING name AS vis_type_name
 )
 INSERT INTO panel (name, url_name)
-SELECT dataset_type_name, md5(random()::text)
-FROM inserted_dataset_type;
+SELECT vis_type_name, md5(random()::text)
+FROM inserted_vis_type;
 
-WITH inserted_dataset_type AS (
-    INSERT INTO dataset_type (name) VALUES
-    ('Hierarchy')
-    RETURNING name AS dataset_type_name
+WITH inserted_vis_type AS (
+    INSERT INTO vis_type (name) VALUES
+    ('hierarchy')
+    RETURNING name AS vis_type_name
 )
 INSERT INTO panel (name, url_name)
-SELECT dataset_type_name, md5(random()::text)
-FROM inserted_dataset_type;
+SELECT vis_type_name, md5(random()::text)
+FROM inserted_vis_type;
 
-WITH inserted_dataset_type AS (
-    INSERT INTO dataset_type (name) VALUES
-    ('Parts of a whole')
-    RETURNING name AS dataset_type_name
+WITH inserted_vis_type AS (
+    INSERT INTO vis_type (name) VALUES
+    ('parts of a whole')
+    RETURNING name AS vis_type_name
 )
 INSERT INTO panel (name, url_name)
-SELECT dataset_type_name, md5(random()::text)
-FROM inserted_dataset_type;
+SELECT vis_type_name, md5(random()::text)
+FROM inserted_vis_type;
 
-WITH inserted_dataset_type AS (
-    INSERT INTO dataset_type (name) VALUES
-    ('Relatedness')
-    RETURNING name AS dataset_type_name
+WITH inserted_vis_type AS (
+    INSERT INTO vis_type (name) VALUES
+    ('relatedness')
+    RETURNING name AS vis_type_name
 )
 INSERT INTO panel (name, url_name)
-SELECT dataset_type_name, md5(random()::text)
-FROM inserted_dataset_type;
+SELECT vis_type_name, md5(random()::text)
+FROM inserted_vis_type;
+*/
 
 /* vis --------------------------------------------------------------------- */
 
 CREATE TABLE vis (
     id SERIAL PRIMARY KEY,
-    dataset_type_id INTEGER NOT NULL,
+    vis_type_id INTEGER NOT NULL,
     name TEXT NOT NULL CHECK (name <> ''),
+    max_limit INTEGER NOT NULL,
     UNIQUE (name)
 );
 
--- Inserting new visualization name into story as well
--- TODO: Is there a better way to do this?
+/*
+
+-- TODO: Need to add max limit for ingesting data to each visualization.
+
+INSERT INTO vis (vis_type_id, name) VALUES (1, 'bar chart');
+INSERT INTO vis (vis_type_id, name) VALUES (2, 'spark line')
+INSERT INTO vis (vis_type_id, name) VALUES (2, 'line chart') 
+INSERT INTO vis (vis_type_id, name) VALUES (2, 'heatmap grid') 
+INSERT INTO vis (vis_type_id, name) VALUES (3, 'dendogram (radial)') 
+INSERT INTO vis (vis_type_id, name) VALUES (3, 'dendogram (linear)') 
+INSERT INTO vis (vis_type_id, name) VALUES (3, 'packed circles') 
+INSERT INTO vis (vis_type_id, name) VALUES (3, 'tree list') 
+INSERT INTO vis (vis_type_id, name) VALUES (4, 'pie chart') 
+INSERT INTO vis (vis_type_id, name) VALUES (4, 'donut chart') 
+INSERT INTO vis (vis_type_id, name) VALUES (5, 'node link') 
+INSERT INTO vis (vis_type_id, name) VALUES (5, 'adjacency matrix') 
+*/
+
+/*
+
+-- TODO: Is there a better way to do this than reinserting the same values in the story table?
 
 WITH inserted_vis AS (
-    INSERT INTO vis (dataset_type_id, name) VALUES 
-    (1, 'Bar chart') 
+    INSERT INTO vis (vis_type_id, name) VALUES 
+    (1, 'bar chart') 
     RETURNING name AS vis_name
 )
 INSERT INTO story (name, url_name) 
@@ -92,8 +120,8 @@ SELECT vis_name, md5(random()::text)
 FROM inserted_vis;
 
 WITH inserted_vis AS (
-    INSERT INTO vis (dataset_type_id, name) VALUES 
-    (2, 'Spark line') 
+    INSERT INTO vis (vis_type_id, name) VALUES 
+    (2, 'spark line') 
     RETURNING name AS vis_name
 )
 INSERT INTO story (name, url_name) 
@@ -101,8 +129,8 @@ SELECT vis_name, md5(random()::text)
 FROM inserted_vis;
 
 WITH inserted_vis AS (
-    INSERT INTO vis (dataset_type_id, name) VALUES 
-    (2, 'Line chart') 
+    INSERT INTO vis (vis_type_id, name) VALUES 
+    (2, 'line chart') 
     RETURNING name AS vis_name
 )
 INSERT INTO story (name, url_name) 
@@ -110,8 +138,8 @@ SELECT vis_name, md5(random()::text)
 FROM inserted_vis;
 
 WITH inserted_vis AS (
-    INSERT INTO vis (dataset_type_id, name) VALUES 
-    (2, 'Heatmap grid') 
+    INSERT INTO vis (vis_type_id, name) VALUES 
+    (2, 'heatmap grid') 
     RETURNING name AS vis_name
 )
 INSERT INTO story (name, url_name) 
@@ -119,8 +147,8 @@ SELECT vis_name, md5(random()::text)
 FROM inserted_vis;
 
 WITH inserted_vis AS (
-    INSERT INTO vis (dataset_type_id, name) VALUES 
-    (3, 'Dendogram (radial)') 
+    INSERT INTO vis (vis_type_id, name) VALUES 
+    (3, 'dendogram (radial)') 
     RETURNING name AS vis_name
 )
 INSERT INTO story (name, url_name) 
@@ -128,8 +156,8 @@ SELECT vis_name, md5(random()::text)
 FROM inserted_vis;
 
 WITH inserted_vis AS (
-    INSERT INTO vis (dataset_type_id, name) VALUES 
-    (3, 'Dendogram (linear)') 
+    INSERT INTO vis (vis_type_id, name) VALUES 
+    (3, 'dendogram (linear)') 
     RETURNING name AS vis_name
 )
 INSERT INTO story (name, url_name) 
@@ -137,8 +165,8 @@ SELECT vis_name, md5(random()::text)
 FROM inserted_vis;
 
 WITH inserted_vis AS (
-    INSERT INTO vis (dataset_type_id, name) VALUES 
-    (3, 'Packed circles') 
+    INSERT INTO vis (vis_type_id, name) VALUES 
+    (3, 'packed circles') 
     RETURNING name AS vis_name
 )
 INSERT INTO story (name, url_name) 
@@ -146,8 +174,8 @@ SELECT vis_name, md5(random()::text)
 FROM inserted_vis;
 
 WITH inserted_vis AS (
-    INSERT INTO vis (dataset_type_id, name) VALUES 
-    (3, 'Tree list') 
+    INSERT INTO vis (vis_type_id, name) VALUES 
+    (3, 'tree list') 
     RETURNING name AS vis_name
 )
 INSERT INTO story (name, url_name) 
@@ -155,8 +183,8 @@ SELECT vis_name, md5(random()::text)
 FROM inserted_vis;
 
 WITH inserted_vis AS (
-    INSERT INTO vis (dataset_type_id, name) VALUES 
-    (4, 'Pie chart') 
+    INSERT INTO vis (vis_type_id, name) VALUES 
+    (4, 'pie chart') 
     RETURNING name AS vis_name
 )
 INSERT INTO story (name, url_name) 
@@ -164,8 +192,8 @@ SELECT vis_name, md5(random()::text)
 FROM inserted_vis;
 
 WITH inserted_vis AS (
-    INSERT INTO vis (dataset_type_id, name) VALUES 
-    (4, 'Donut chart') 
+    INSERT INTO vis (vis_type_id, name) VALUES 
+    (4, 'donut chart') 
     RETURNING name AS vis_name
 )
 INSERT INTO story (name, url_name) 
@@ -173,8 +201,8 @@ SELECT vis_name, md5(random()::text)
 FROM inserted_vis;
 
 WITH inserted_vis AS (
-    INSERT INTO vis (dataset_type_id, name) VALUES 
-    (5, 'Node link') 
+    INSERT INTO vis (vis_type_id, name) VALUES 
+    (5, 'node link') 
     RETURNING name AS vis_name
 )
 INSERT INTO story (name, url_name) 
@@ -182,17 +210,17 @@ SELECT vis_name, md5(random()::text)
 FROM inserted_vis;
 
 WITH inserted_vis AS (
-    INSERT INTO vis (dataset_type_id, name) VALUES 
-    (5, 'Adjacency matrix') 
+    INSERT INTO vis (vis_type_id, name) VALUES 
+    (5, 'adjacency matrix') 
     RETURNING name AS vis_name
 )
 INSERT INTO story (name, url_name) 
 SELECT vis_name, md5(random()::text)
 FROM inserted_vis;
+*/
 
-/* */
+/* story_vis --------------------------------------------------------------- */
 
-/* TODO */
 CREATE TABLE story_vis ( 
     id SERIAL PRIMARY KEY,
     story_id INTEGER NOT NULL,
@@ -201,4 +229,124 @@ CREATE TABLE story_vis (
     UNIQUE (story_id, vis_id)
 );
 
-INSERT INTO story_vis (story_id, vis_id, order_num) VALUES ('1' , '1') ON DUPLICATE story_id UPDATE order_num = order_num + 1;
+-- TODO: add data
+-- INSERT INTO story_vis (story_id, vis_id, order_num) VALUES (1, 1, 1);
+
+/* vis's attributes -------------------------------------------------------- */
+
+CREATE TABLE vis_code_attr (
+    vis_id INTEGER PRIMARY KEY,
+    viz_data TEXT DEFAULT NULL,
+    is_viz_data_required BOOLEAN DEFAULT FALSE,
+    canvas_width TEXT DEFAULT NULL,
+    is_canvas_width_required BOOLEAN DEFAULT FALSE,
+    canvas_height TEXT DEFAULT NULL,
+    is_canvas_height_required BOOLEAN DEFAULT FALSE,
+    orientation TEXT DEFAULT NULL,
+    is_orientation_required BOOLEAN DEFAULT FALSE,
+    format TEXT DEFAULT NULL,
+    is_format_required BOOLEAN DEFAULT FALSE,
+    grouping TEXT DEFAULT NULL,
+    is_grouping_required BOOLEAN DEFAULT FALSE,
+    start_group TEXT DEFAULT NULL,
+    is_start_group_required BOOLEAN DEFAULT FALSE
+);
+
+-- TODO: add more data
+
+INSERT INTO vis_code_attr (vis_id, canvas_width, canvas_height, orientation) VALUES 
+    (1, 'integer', 'integer', 'vertical | horizontal');
+INSERT INTO vis_code_attr (vis_id) VALUES 
+    (2);
+INSERT INTO vis_code_attr (vis_id) VALUES 
+    (3);
+INSERT INTO vis_code_attr (vis_id) VALUES 
+    (4);
+INSERT INTO vis_code_attr (vis_id, viz_data, is_viz_data_required, canvas_width, canvas_height, format) VALUES 
+    (5, 'dataset', TRUE, 'integer', 'integer', 'radial | linear');
+INSERT INTO vis_code_attr (vis_id, viz_data, is_viz_data_required, canvas_width, canvas_height) VALUES 
+    (6, 'dataset', TRUE, 'integer', 'integer');
+INSERT INTO vis_code_attr (vis_id, viz_data, is_viz_data_required, canvas_width, canvas_height) VALUES 
+    (7, 'dataset', TRUE, 'integer', 'integer');
+INSERT INTO vis_code_attr (vis_id, viz_data, is_viz_data_required) VALUES 
+    (8, 'dataset', TRUE);
+INSERT INTO vis_code_attr (vis_id) VALUES 
+    (9);
+INSERT INTO vis_code_attr (vis_id) VALUES 
+    (10);
+INSERT INTO vis_code_attr (vis_id) VALUES 
+    (11);
+INSERT INTO vis_code_attr (vis_id) VALUES 
+    (12);
+
+CREATE TABLE vis_do_attr (
+    vis_id INTEGER PRIMARY KEY,
+    do_value TEXT NOT NULL CHECK (do_value <> '')
+    UNIQUE (vis_id, do_value)
+);
+
+-- TODO: add more data
+
+INSERT INTO vis_do_attr (vis_id, do_value) VALUES
+    (5, 'Use for data that is hierarchial.');
+INSERT INTO vis_do_attr (vis_id, do_value) VALUES
+    (5, 'Use when space is restricted and generally square shaped.');
+INSERT INTO vis_do_attr (vis_id, do_value) VALUES
+    (6, 'Use for data that is hierarchial.');
+INSERT INTO vis_do_attr (vis_id, do_value) VALUES
+    (6, 'Use when data is more than 6 layers deep or data has more than 100 nodes.');
+INSERT INTO vis_do_attr (vis_id, do_value) VALUES
+    (7, 'Use for data that is hierarchial.');
+INSERT INTO vis_do_attr (vis_id, do_value) VALUES
+    (7, 'Use when data is more than 6 layers deep or data has more than 100 nodes.');
+INSERT INTO vis_do_attr (vis_id, do_value) VALUES
+    (8, 'Use for data that is hierarchial.');
+INSERT INTO vis_do_attr (vis_id, do_value) VALUES
+    (8, 'Use when data is more than 6 layers deep or data has more than 100 nodes.');
+INSERT INTO vis_do_attr (vis_id, do_value) VALUES
+    (8, 'Use when space is extremely limited and/or extremely tall/skinny.');
+
+CREATE TABLE vis_dont_attr (
+    vis_id INTEGER PRIMARY KEY,
+    dont_value TEXT NOT NULL CHECK (dont_value <> '')
+    UNIQUE (vis_id, dont_value)
+);
+
+-- TODO: add more data
+
+INSERT INTO vis_dont_attr (vis_id, dont_value) VALUES
+    (5, 'Don\'t use if data is more than 6 layers deep.');
+INSERT INTO vis_dont_attr (vis_id, dont_value) VALUES
+    (5, 'Don\'t use if node labels are longer than 2 words.');
+INSERT INTO vis_dont_attr (vis_id, dont_value) VALUES
+    (5, 'Don\'t use if the difference between the number of nodes between layers is drastic.');
+INSERT INTO vis_dont_attr (vis_id, dont_value) VALUES
+    (6, 'Don\'t use if the entire visual does not fit in available space without scrolling.');
+INSERT INTO vis_dont_attr (vis_id, dont_value) VALUES
+    (7, 'Don\'t use it if interactivity is not available.');
+INSERT INTO vis_dont_attr (vis_id, dont_value) VALUES
+    (7, 'Don\'t use when all labels must be visible at once.');
+INSERT INTO vis_dont_attr (vis_id, dont_value) VALUES
+    (8, 'Don\'t use it if interactivity is not available.');
+INSERT INTO vis_dont_attr (vis_id, dont_value) VALUES
+    (8, 'Don\'t use when all labels must be visible at once.');
+
+CREATE TABLE vis_alternative_attr (
+    vis_id INTEGER PRIMARY KEY,
+    alt_vis_id INTEGER NOT NULL,
+    UNIQUE (vis_id, alt_vis_id)
+);  
+
+-- TODO: add more data
+
+INSERT INTO vis_alternative_attr (vis_id, alt_vis_id) VALUES (5, 7);
+INSERT INTO vis_alternative_attr (vis_id, alt_vis_id) VALUES (5, 8);
+INSERT INTO vis_alternative_attr (vis_id, alt_vis_id) VALUES (6, 7);
+INSERT INTO vis_alternative_attr (vis_id, alt_vis_id) VALUES (6, 8);
+INSERT INTO vis_alternative_attr (vis_id, alt_vis_id) VALUES (7, 5);
+INSERT INTO vis_alternative_attr (vis_id, alt_vis_id) VALUES (7, 6);
+INSERT INTO vis_alternative_attr (vis_id, alt_vis_id) VALUES (7, 8);
+INSERT INTO vis_alternative_attr (vis_id, alt_vis_id) VALUES (8, 5);
+INSERT INTO vis_alternative_attr (vis_id, alt_vis_id) VALUES (8, 6);
+INSERT INTO vis_alternative_attr (vis_id, alt_vis_id) VALUES (8, 7);
+
