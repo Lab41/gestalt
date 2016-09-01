bb9257c888034e8fcc0da48c6488e07161b226b9
Tiffany J <tiffapedia@gmail.com>
2016-09-01 10:28:04 -0700

    wrap all services in IIFE

--- prototypes/desktop/www/js/services/agent-service.js
+++ prototypes/desktop/www/js/services/agent-service.js
@@ -1,50 +1,64 @@
-angular.module("agent-service", [])
+// wrap in Immediately Invoked Function Expression to avoid global scope 
+(function() {
+    'use strict';
 
-.factory("agentService", ["$http", "$q", function($http, $q) {
-		
-	return {
-		
-		// data storage
-		content: "",
-		
-		// single request stored in a promise
-		makeRequest: function() {
+    // set agent-service application and register its service
+    angular
+        .module("agent-service", [])
+        .factory("agentService", agentService);
+
+    // add additional services to be used within the service
+    agentService.$inject = ["$http", "$q"];
+
+    // define the service
+    function agentService($http, $q) {
+
+        // return an agentService instance			
+		return {
 			
-			// create deferred object
-			var deferred = $q.defer();
+			// data storage
+			content: "",
 			
-			// TODO construct clean data obj for relevant user agent info
-			// for now all we need is the device info
-			
-			// make request
-			var info = navigator.userAgent.split(" ");
-			var obj = {};
-			obj["os"] = info[2] == "Android" ? info[2] : info[3];
-			
-			deferred.resolve(obj);
-			
-			// expose the promise data
-			return deferred.promise;
-			
-		},
-		
-		// unique data requests
-		getData: function() {
-			
-			// check for existing stored data
-			if (!this.content) {
-			
-				// make request
-				//console.log("****** GET " + navigator.userAgent + " ******");
-				this.content = this.makeRequest();
+			// single request stored in a promise
+			makeRequest: function() {
 				
-			};
+				// create deferred object
+				var deferred = $q.defer();
+				
+				// TODO construct clean data obj for relevant user agent info
+				// for now all we need is the device info
+				
+				// make request
+				var info = navigator.userAgent.split(" ");
+				var obj = {};
+				obj["os"] = info[2] == "Android" ? info[2] : info[3];
+				
+				deferred.resolve(obj);
+				
+				// expose the promise data
+				return deferred.promise;
+				
+			},
 			
-			// return stored data
-			return this.content;
+			// unique data requests
+			getData: function() {
+				
+				// check for existing stored data
+				if (!this.content) {
+				
+					// make request
+					//console.log("****** GET " + navigator.userAgent + " ******");
+					this.content = this.makeRequest();
+					
+				};
+				
+				// return stored data
+				return this.content;
+				
+			}
 			
-		}
-		
-	};
+		};
 	
-}]);
+    }
+
+})();
--- prototypes/desktop/www/js/services/d3-service.js
+++ prototypes/desktop/www/js/services/d3-service.js
@@ -1,46 +1,59 @@
-angular.module("d3-service", [])
+// wrap in Immediately Invoked Function Expression to avoid global scope 
+(function() {
+    'use strict';
 
-.factory("d3Service", ["$document", "$window", "$q", "$rootScope", function($document, $window, $q, $rootScope) {
-	
-	var d = $q.defer();
-	var d3service = {
-		d3: function() {
-			return d.promise;
-		}
-	};
-	
-	//create script tag for d3 source
-	var scriptTag = $document[0].createElement("script");
-	scriptTag.type = "text/javascript";
-	scriptTag.async = true;
-	scriptTag.src = "lib/d3.js";
-	scriptTag.onreadystatechange = function() {
+    // set d3-service application and register its service
+    angular
+        .module("d3-service", [])
+        .factory("d3Service", d3Service);
+
+    // add additional services to be used within the service
+    d3Service.$inject = ["$document", "$q", "$rootScope", "$window"];
+
+    // define the service
+    function d3Service($document, $q, $rootScope, $window) {
+    	
+		var d = $q.defer();
+		var d3service = {
+			d3: function() {
+				return d.promise;
+			}
+		};
 		
-		//check state
-		if(this.readyState == "complete") {
+		//create script tag for d3 source
+		var scriptTag = $document[0].createElement("script");
+		scriptTag.type = "text/javascript";
+		scriptTag.async = true;
+		scriptTag.src = "lib/d3.js";
+		scriptTag.onreadystatechange = function() {
 			
-			onScriptLoad();
+			//check state
+			if(this.readyState == "complete") {
+				
+				onScriptLoad();
+				
+			};
 			
 		};
 		
-	};
-	
-	scriptTag.onload = onScriptLoad;
-	
-	//add script tage to document
-	var s = $document[0].getElementsByTagName("body")[0];
-	s.appendChild(scriptTag);
-	
-	//return d3 object
-    return d3service;
-	
-	function onScriptLoad() {
+		scriptTag.onload = onScriptLoad;
 		
-		//load client in the browser
-		$rootScope.$apply(function() {
-			d.resolve($window.d3);
-		});
+		//add script tage to document
+		var s = $document[0].getElementsByTagName("body")[0];
+		s.appendChild(scriptTag);
 		
-	};
+		//return d3 object
+	    return d3service;
+		
+		function onScriptLoad() {
+			
+			//load client in the browser
+			$rootScope.$apply(function() {
+				d.resolve($window.d3);
+			});
+			
+		};
 	
-}]);
+    }
+
+})();
