9f93bcc4592a3f13e0306fb87be990269aa59d9f
Tiffany J <tiffapedia@gmail.com>
2016-08-31 17:20:04 -0700

    minor update to layout-service.js

--- prototypes/desktop/www/js/controllers/login-controller.js
+++ prototypes/desktop/www/js/controllers/login-controller.js
@@ -2,7 +2,7 @@
 (function() {
     'use strict';
 
-    // set login-controller and register its controller
+    // set login-controller application and register its controller
     angular
         .module("login-controller", [])
         .controller("loginController", loginController);
@@ -10,31 +10,11 @@
     // add additional services to be used within the controller
     loginController.$inject = ["$scope", "$state", "authenticationService", "layoutService"];
 
-<<<<<<< 425b1cd370d0917d34c35e71b992e6be31f9a305
-<<<<<<< da29128115c3ae1583d446f2b908a1d84c2660d0
-                // transition to default workspace
-                $state.go("app.panel.visual", {
-                    workspace: workspace.url_name,
-                    panel: workspace.default_panel,
-                    visual: workspace.default_vis
-                });
-=======
-loginCtrl.prototype.login = function(personaId, personaName) {
-    var _this = this;
-=======
     // define the controller
     function loginController($scope, $state, authenticationService, layoutService) {
->>>>>>> clean up front end login-controller and its respective services
-
-        // define bindable members
         $scope.listOfPersonas;
         $scope.login = login;
-        
-<<<<<<< 425b1cd370d0917d34c35e71b992e6be31f9a305
-        // get single workspace
-        _this.layoutService.getStructure(true, objs, backendUrl, check).then(function(singleWorkspace) {
->>>>>>> avoiding scope soup
-=======
+
         // call functions
         getListOfPersonas();
 
@@ -54,7 +34,6 @@ loginCtrl.prototype.login = function(personaId, personaName) {
             var check = { key: "is_default", value: true };
             
             layoutService.getStructure(true, objs, endpoint, check).then(function(singleWorkspace) {
->>>>>>> clean up front end login-controller and its respective services
 
                 var workspace = singleWorkspace;
 
