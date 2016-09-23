c1fd829976870b086766f27f49048552db0fe125
wesa <wesa@random.com>
2016-09-23 11:07:43 -0700

    added comments for screenshot.py

--- prototypes/desktop/screenshot.py
+++ prototypes/desktop/screenshot.py
@@ -28,29 +28,22 @@ class postViewport:
         
         # get values from post
         url_data = ast.literal_eval(web.data())
-        
         width = url_data["width"]
-        #print width
         height = url_data["height"]
-        #print height
         web_url = url_data["url"]
-        #print url
+        # create image url
         timestr = time.strftime("%Y%m%d-%H%M%S")
-        #print timestr
-
         current_dir =  os.path.abspath(os.path.dirname(__file__))
-        parent_dir = os.path.abspath(current_dir + "/../")
-        print current_dir
-        print parent_dir
         img_name = "screenshot-"+timestr
         img_url = current_dir+"/www/"+img_name+'.png'
-        print img_url
         # insert code into screenshot.js
         fo=open("screenshot.js","wb")
         code="var page = require('webpage').create(); page.viewportSize = { width: "+str(width)+", height: "+str(height)+" }; page.open('"+ web_url+"', function(status) { setTimeout(function(){ page.render('"+img_url+"'); console.log('completed'); phantom.exit(); },1000); });"
         fo.write(code)
         fo.close()
+        # execute command to take screeshot
         os.system('phantomjs screenshot.js')
+        # send back url,so that user can download image.
         return "http://0.0.0.0:8000/"+img_name+".png"
 
 # instantiate the application
