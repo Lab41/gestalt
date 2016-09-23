19255f5cda79dc70984879f140cc91f24e9e7641
Joel N <joeln@DEV-LT-5663.local>
2016-09-23 11:40:44 -0700

    Added a highlight service

--- prototypes/desktop/www/js/desktop.services.js
+++ prototypes/desktop/www/js/desktop.services.js
@@ -1,12 +1,13 @@
 angular.module("desktop.services", [
     "layout-service",
-	"authentication-service",
-	"base64-service",
-	"content-service",
+    "authentication-service",
+    "base64-service",
+    "content-service",
     "plotly-service",
-	"d3-service",
-	"mapbox-service",
-	"agent-service",
-    "economic-service",,
-    "screenshot-service"
+    "d3-service",
+    "mapbox-service",
+    "agent-service",
+    "economic-service",
+    "screenshot-service",
+    "highlight-service"
 ]);
