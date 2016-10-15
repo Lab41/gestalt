b38099bc0f9c045f19c4b6bebf984766b0d2009c
Lindsay Gensinger <lindsay@Lindsays-MacBook.local>
2016-10-15 00:32:57 -0700

    small fixes

--- prototypes/tablet/www/js/controllers/viz-controller.js
+++ prototypes/tablet/www/js/controllers/viz-controller.js
@@ -1,35 +1,35 @@
 angular.module("viz-controller", [])
 
 .controller("vizCtrl", ["$scope", "contentService", "$state", "$rootScope", function($scope, contentService, $state, $rootScope) {
-	        
+    	        
 	// data objects
-	$scope.nodes;
 	$scope.nodeGroups;
-	$scope.dynamicDirectives;
-    
-    // country nodes
-	contentService.getData("visualization/cdis").then(function(data) {
-		
-		// set scope
-		$scope.nodes = data;
-		
-	});
+	$scope.heuristics;
+	$scope.tileGridData;
+	$scope.boundaryData;
 	
 	// node groups
-	contentService.getData("visualization/countries/groups").then(function(data) {
+	contentService.getData("visualization/countries/groups/").then(function(data) {
 		
 		// set scope
 		$scope.nodeGroups = data;
 		
 	});
 	
-	$scope.dummyData = [{"name": "category1", "value": 5}, {"name": "category2", "value": 2}, {"name": "category3", "value": 1}];
-	
-	// dynamic directives
-    contentService.getData("visualization/angular/directives/1").then(function(data) {
+	// geojson
+	contentService.getData("visualization/geography/geojson/hexagon/").then(function(data) {
 		
 		// set scope
-		$scope.dynamicDirectives = data;
+		$scope.tileGridData = data;
+		
+	});
+
+
+	// geojson
+	contentService.getData("visualization/geography/geojson/boundary/").then(function(data) {
+		
+		// set scope
+		$scope.boundaryData = data;
 		
 	});
     
